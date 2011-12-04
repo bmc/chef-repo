2e4f25e9d0b9639d666dbde2b95cb66cd86f36dc
Brian Clapper <bmc@ardentex.com>
2011-12-04 17:57:04 -0500

    metadata mod

--- cookbooks/jdk6/metadata.rb
+++ cookbooks/jdk6/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Brian M. Clapper"
 maintainer_email  "bmc@clapper.org"
 license           "BSD"
-description       "Installs Java 6 JDK on Debian or Ubunut"
+description       "Installs Java 6 JDK on Debian or Ubuntu"
 version           "0.1"
 
 recipe "jdk6", "Installs Java 6 JDK"
