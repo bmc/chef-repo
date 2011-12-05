8e64fdfa5714a0628ca977dec2b4039d0ec73143
Brian Clapper <bmc@ardentex.com>
2011-12-04 22:59:37 -0500

    Added guard

--- cookbooks/virtual-python/recipes/default.rb
+++ cookbooks/virtual-python/recipes/default.rb
@@ -5,4 +5,5 @@ bash "installing Python virtual environment" do
   user node[:virtualpy][:user]
   code "virtualenv #{node[:virtualpy][:dir]}"
   only_if { node[:virtualpy][:user] && node[:virtualpy][:dir] }
+  not_if "test -e #{node[:virtualpy][:dir]}"
 end
