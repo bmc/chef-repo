adfa64051db4b0a1b4665b7948b6c0d6a1da12b9
Brian Clapper <bmc@ardentex.com>
2011-12-05 10:26:54 -0500

    Cleaned up rvm and ruby recipes so they only run when necessary.

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -5,4 +5,5 @@ bash "installing user-local RVM" do
   export HOME="/home/#{node[:rvm][:user]}"
   bash < <( curl -L https://rvm.beginrescueend.com/install/rvm)
   EOF
+  not_if "test -d /home/#{node[:rvm][:user]}/.rvm"
 end
--- cookbooks/rvm/recipes/ruby.rb
+++ cookbooks/rvm/recipes/ruby.rb
@@ -1,12 +1,22 @@
 include_recipe "build-essential"
 
+cookbook_file "/home/#{node[:rvm][:user]}/.rvm/bin/rvm-ruby-installed" do
+  source "rvm-ruby-installed"
+  owner  node[:rvm][:user]
+  mode   0755
+end
+
 bash "installing Ruby via RVM" do
-  user "#{node[:rvm][:user]}"
+  user node[:rvm][:user]
   code <<-EOF
   export HOME=$(echo ~#{node[:rvm][:user]})
   export PATH=$PATH:$HOME/.rvm/bin
-  rvm pkg install zlib
-  rvm install #{node[:rvm][:ruby]} --with-zlib-dir=$HOME/.rvm/usr
+  if /home/#{node[:rvm][:user]}/.rvm/bin/rvm-ruby-installed #{node[:rvm][:ruby]}
+  then
+      :
+  else
+      rvm pkg install zlib
+      rvm install #{node[:rvm][:ruby]} --with-zlib-dir=$HOME/.rvm/usr
+  fi
   EOF
-  only_if { node[:rvm][:user] && node[:rvm][:ruby] }
 end
