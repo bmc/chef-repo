ee8cdc8bc26356e87094bbf1c8c44f77f2f04331
Brian Clapper <bmc@ardentex.com>
2011-12-10 16:52:30 -0500

    Added linux-accounts

---
+++ cookbooks/linux-accounts/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Brian M. Clapper"
+maintainer_email  "bmc@clapper.org"
+license           "BSD"
+description       "Installs extra accounts on Linux systems. Uses node[:vm_user]"
+version           "0.1"
+
+recipe "linux-accounts", "Some Linux accounts"
