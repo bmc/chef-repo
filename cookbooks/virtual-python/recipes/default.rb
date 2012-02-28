54ccbc8f3883014fca4c850f351a10df5b23292d
Brian Clapper <bmc@ardentex.com>
2012-02-28 15:09:32 -0500

    Adjusted virtualenv

--- cookbooks/virtual-python/recipes/default.rb
+++ cookbooks/virtual-python/recipes/default.rb
@@ -3,7 +3,10 @@ package "python-virtualenv"
 
 bash "installing Python virtual environment" do
   user node[:virtualpy][:user]
-  code "virtualenv #{node[:virtualpy][:dir]}"
+  code <<-EOF
+  export PATH=/usr/bin:/bin:/usr/local/bin
+  virtualenv #{node[:virtualpy][:dir]}
+  EOF
   only_if { node[:virtualpy][:user] && node[:virtualpy][:dir] }
   not_if "test -e #{node[:virtualpy][:dir]}"
 end
